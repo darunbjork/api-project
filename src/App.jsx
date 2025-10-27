bd75c75a1af40d20e8a1a025ad249efa83e1c04d
darunbjork <darunbjork@gmail.com>
2025-10-27 12:21:40 +0100

    render text

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
-import { useState } from "react";
-
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <h1>You clicked {count} times</h1>
-      <button onClick={() => setCount(count + 1)}>Count + 1</button>
-      <button onClick={() => setCount(count - 1)}>Count - 1</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-    </div>
-  );
+  return <h1>My API Project</h1>;
 }
 
 export default App;
