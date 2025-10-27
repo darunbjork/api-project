e8d84c215c9182b03e1405de1d529db589400051
darunbjork <darunbjork@gmail.com>
2025-10-27 13:35:30 +0100

    useEffect added to auto-fetch API data on initial render

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
@@ -7,28 +7,30 @@ function App() {
 
   const fetchData = async () => {
     setLoading(true);
-
     try {
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/users"
       );
-
       const result = await response.json();
-
       setData(result);
       setLoading(false);
     } catch (error) {
-      console.log("Something went wrong", error);
+      console.log("Error fetching data, try again", error);
       setLoading(false);
     }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>My API Project</h1>
-      <p>Data {data.length} items</p>
+      <h1>My API project</h1>
       <button onClick={fetchData}>Get Data</button>
-      <p>Loading {loading ? "Yes" : "No"}</p>
+
+      <h3>Loading {loading && <p>...Loading</p>}</h3>
+      <h3>{!loading && <p> Data {data.length} users</p>}</h3>
     </div>
   );
 }
