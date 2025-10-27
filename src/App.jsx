fd3b43920137133f492ee61c72efd9b588a3509d
darunbjork <darunbjork@gmail.com>
2025-10-27 12:33:44 +0100

    count state update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+
 function App() {
-  return <h1>My API Project</h1>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>My API Project</h1>
+      <h3>Count is {count}</h3>
+      <button onClick={() => setCount(count + 1)}>Count +</button>
+    </div>
+  );
 }
 
 export default App;
