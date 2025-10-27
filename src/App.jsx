db3bc0690e9448e235f69d14ee9a511f178dae80
darunbjork <darunbjork@gmail.com>
2025-10-27 12:42:44 +0100

    packe React boxes for data and loading state

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div>
       <h1>My API Project</h1>
       <h3>Count is {count}</h3>
+      <p>Data {data.length} items</p>
+      <p>Loading {loading ? "Yes" : "No"}</p>
       <button onClick={() => setCount(count + 1)}>Count +</button>
     </div>
   );
