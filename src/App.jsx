2f39fed0c2d8da82fa0f014b61308694a73dc7ea
darunbjork <darunbjork@gmail.com>
2025-10-27 13:02:47 +0100

    add fetchData function to retrieve user data from JSONPlaceholder API

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      const result = await response.json();
+
+      setData(result);
+      setLoading(false);
+    } catch (error) {
+      console.log("Something went wrong", error);
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
       <h1>My API Project</h1>
-      <h3>Count is {count}</h3>
       <p>Data {data.length} items</p>
+      <button onClick={fetchData}>Get Data</button>
       <p>Loading {loading ? "Yes" : "No"}</p>
-      <button onClick={() => setCount(count + 1)}>Count +</button>
     </div>
   );
 }
